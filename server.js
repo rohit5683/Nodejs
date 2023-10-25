05ea75ffe4b45a79fac1dbf9f0c4d8b525f0ff72
Rohit Vishwakarma <125693200+rohit5683@users.noreply.github.com>
2023-10-25 07:19:52 +0000

    Server is ready

--- server.js
+++ server.js
@@ -10,11 +10,21 @@ const app = express()
 //middleware 
 app.use(express())
 app.use(express.json())
+app.use(morgan("dev"))
+app.use(bodyParser.urlencoded({extended:false}))
 
 
 //Server route
 app.get("/", (req, res)=>{
     res.send("<h1>Welcome to server<h1/>")
+})
+
+app.post("/contact-form",(req, res)=>{
+    const {inputData} = req.body
+    res.json({
+        success: true,
+        message: `Welcome to ${inputData}`
+    })
 })
 
 
